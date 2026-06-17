d9de1c152e484c74aefe5c445f4f025be99244ca
Monney Christopher <monneychristopher4197@gmail.com>
2026-06-17 21:53:19 +0000

    Added C++ Assignment : Greater than operation

---
+++ Greater_than.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Enter first integer: ";
+    cin >> a;
+    
+    cout<< "Enter first integer: ";
+    cin >> b;
+
+    if (a > b)
+        cout << "First value is greater";
+    else
+        cout << "Second value is greater";
+
+    return 0;
+}
