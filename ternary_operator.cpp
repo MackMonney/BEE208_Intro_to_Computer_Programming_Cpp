fbcb9706a6a91e3fb6569728f9e7f436b2c8f6e4
Monney Christopher <monneychristopher4197@gmail.com>
2026-06-18 01:44:46 +0000

    Adding C++ Assignment : Ternary Operator

---
+++ ternary_operator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Enter first integer: ";
+    cin >> a;
+    
+    cout << "Enter second integer: ";
+    cin >> b;
+
+    int larger = (a > b) ? a : b;
+
+    cout << "Larger number = " << larger << endl;
+
+    return 0;
+}
