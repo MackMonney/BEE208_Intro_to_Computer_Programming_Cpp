d02b6b2adf7194a46e1e5091a24f4cd66e7b64f4
Monney Christopher <monneychristopher4197@gmail.com>
2026-06-18 01:43:55 +0000

    Swap the values of two variables

---
+++ swap_variables.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Enter first number: ";
+    cin >> a;
+    
+    cout << "Enter second number: ";
+    cin >> b;
+
+    cout << "Before swapping:" << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+
+    cout << "After swapping:" << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    return 0;
+}
